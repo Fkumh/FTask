56fc0f1bb644203549240ec61ae1973157d35a48
Ivan <Iv.Zavyalov07@gmail.com>
2022-12-08 21:21:22 +0500

    FT

---
+++ Desktop/FinalTask/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array1 = new string[5] {"Kazan", "DF", "Moscow", "98941969489", "WOW"};
+string[] array2 = new string[array1.Length];
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length) System.Console.Write($"{array[i]} ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+void CheckArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+CheckArray(array1, array2);
+PrintArray(array2);
